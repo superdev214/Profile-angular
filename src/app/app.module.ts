751a7f5680ad2b62776f1f0215b053f42d0ab74b
superdev <superdev0241002@gmail.com>
2022-11-21 18:11:35 +0900

    A feat: Add ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UIRouterModule } from "@uirouter/angular";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NavComponent } from './component/nav/nav.component';
 
+const helloState = { name: "hello", url: "/", component: HomeComponent };
+const aboutState = { name: "about", url: "/About", component: AboutComponent };
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { NavComponent } from './component/nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({ states: [helloState, aboutState], useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
